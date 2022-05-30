f1950f2ac4514aadbf603657eef0e20e7ec3232d
psw4887 <83569822+psw4887@users.noreply.github.com>
2022-05-30 16:26:39 +0900

    필요없는 파일 삭제

--- src/main/java/com/nhnacademy/security/repository/MemberRepository.java
+++ src/main/java/com/nhnacademy/security/repository/MemberRepository.java
@@ -4,5 +4,4 @@ import com.nhnacademy.security.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
-    boolean existsByMemberIdAndPw(String id, String pw);
 }
